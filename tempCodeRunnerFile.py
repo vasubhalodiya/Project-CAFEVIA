bcc440061f1ac709c75137dd2873cb4dacde80b9
Vishal Nathani <vishal.nathani17@gmail.com>
2024-12-04 21:05:32 +0530

    update

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-/loginscreen1.png")
-        # img = ImageTk.PhotoImage(img)
-        # panel = Label(loginframe_sub, image=img,height=360,width=400,bg='white')
-        # panel.image = img
-        # panel.place(x=0,y=150)
